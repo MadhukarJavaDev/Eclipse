b04475b171b2d2aaa563313ea717d90583343432
MadhukarJavaDev <madhukar.javadev@gmail.com>
2019-04-15 04:20:54 +0530

    for testing

---
+++ basic/src/basicJava/HelloWorld.java
@@ -0,0 +1,11 @@
+package basicJava;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("How you doing");
+
+	}
+
+}
